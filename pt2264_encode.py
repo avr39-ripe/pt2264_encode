ab1eba825eab70acd95985b03ba42b28ae02204e
Alexander V. Ribchansky <avr39ripe@gmail.com>
2014-04-09 00:53:20 +0300

    f*king ident and sync pulse :(

--- pt2264_encode.py
+++ pt2264_encode.py
@@ -9,13 +9,13 @@ HIGH = 1
 
 xmtrPin = 0
 
-#PULSESHORT = 500
-#PULSELONG = 1500
-#PULSESYNC = 16000
+PULSESHORT = 500
+PULSELONG = 1500
+PULSESYNC = 16000
 
-PULSESHORT = 160
-PULSELONG = 500
-PULSESYNC = 5333
+#PULSESHORT = 160
+#PULSELONG = 500
+#PULSESYNC = 5333
 
 wiringPiSetup()
 
@@ -35,23 +35,28 @@ def pt2262Send(packet):
         for x in range(0, 12):
 
             if packet[x] == '0':
+                print '0'
                 ookPulse(PULSESHORT,PULSELONG)
                 ookPulse(PULSESHORT,PULSELONG)
             elif packet[x] == '1':
+                print '1'
                 ookPulse(PULSELONG,PULSESHORT)
                 ookPulse(PULSELONG,PULSESHORT)
             elif packet[x] == 'F' or packet[x] == 'f':
+                print 'f'
                 ookPulse(PULSESHORT,PULSELONG)
                 ookPulse(PULSELONG,PULSESHORT)
             elif packet[x] == 'U' or packet[x] == 'u':
+                print 'u'
                 ookPulse(PULSELONG,PULSESHORT)
                 ookPulse(PULSESHORT,PULSELONG)
 
-            ookPulse(PULSESHORT,PULSESYNC)
+        ookPulse(PULSESHORT,PULSESYNC)
+        print 'sync'
 
 if __name__ == "__main__":
     while True:
         pt2262Send("FFFFF0FF0001")
-        pt2262Send("1000FF0FFFFF")
+#        pt2262Send("1000FF0FFFFF")
 
 
